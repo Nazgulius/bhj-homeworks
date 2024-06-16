d6809aae66eb74a698352115232620476e252697
Makis <Nazgulius@yandex.ru>
2024-06-16 11:24:15 +0300

    Add files via upload

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -2,39 +2,38 @@ let tab = document.getElementsByClassName('tab');
 let tabActive = document.getElementsByClassName('tab_active');
 let tabContent = document.getElementsByClassName('tab__content');
 
-
-let i = 0;
-
 tab.item(0).addEventListener(  
   "click",    
-  () => {      
-    console.log(tab.indexOf('tab_active'));
-    i = tab.indexOf(tabActive);
-    tab.item(i).classList.toggle ('tab_active');
-    tabContent.item(i).classList.toggle ('tab__content_active');
-    tab.item(0).classList.toggle ('tab_active');
-    tabContent.item(0).classList.toggle ('tab__content_active');
+  () => { 
+    tab.item(0).classList.toggle ('tab_active', true);
+    tab.item(1).classList.toggle ('tab_active', false);
+    tab.item(2).classList.toggle ('tab_active', false);
+    tabContent.item(0).classList.toggle ('tab__content_active', true);
+    tabContent.item(1).classList.toggle ('tab__content_active', false);
+    tabContent.item(2).classList.toggle ('tab__content_active', false);
   }  
 )
 
 tab.item(1).addEventListener(
   "click",
-  function () {
-    i = tab.indexOf('tab_active');
-    tab.item(i).classList.toggle ('tab_active');
-    tabContent.item(i).classList.toggle ('tab__content_active');
-    tab.item(1).classList.toggle ('tab_active');
-    tabContent.item(1).classList.toggle ('tab__content_active');
-  }
+  () => { 
+    tab.item(0).classList.toggle ('tab_active', false);
+    tab.item(1).classList.toggle ('tab_active', true);
+    tab.item(2).classList.toggle ('tab_active', false);
+    tabContent.item(0).classList.toggle ('tab__content_active', false);
+    tabContent.item(1).classList.toggle ('tab__content_active', true);
+    tabContent.item(2).classList.toggle ('tab__content_active', false);
+  }  
 )
 
 tab.item(2).addEventListener(
   "click",
-  () => {
-    i = tab.indexOf(tabActive);
-        tab.item(i).classList.toggle ('tab_active');
-        tabContent.item(i).classList.toggle ('tab__content_active');
-    tab.item(2).classList.toggle ('tab_active');
-    tabContent.item(2).classList.toggle ('tab__content_active');
-  }
+  () => { 
+    tab.item(0).classList.toggle ('tab_active', false);
+    tab.item(1).classList.toggle ('tab_active', false);
+    tab.item(2).classList.toggle ('tab_active', true);
+    tabContent.item(0).classList.toggle ('tab__content_active', false);
+    tabContent.item(1).classList.toggle ('tab__content_active', false);
+    tabContent.item(2).classList.toggle ('tab__content_active', true);
+  }  
 )
