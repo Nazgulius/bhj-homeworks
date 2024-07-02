c59d0862ea85da17223ada1c0187ce687f2b80d2
Maksim Chalyh <nazgulius@yandex.ru>
2024-07-02 23:35:07 +0300

    document-structure begin

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,14 +1,63 @@
-let hasTooltip = document.querySelectorAll('.has-tooltip')[0];
-let elementHelp = dovument.createElement('<div class="tooltip">Текст подсказки</div');
+/*let hasTooltip;
+for (let i = 0; i < hasTooltip.length; i++) {
+  document.querySelectorAll('.has-tooltip')[i];
+  hasTooltip.insertAdjacentHTML('beforeEnd','<div class="tooltip">Текст подсказки</div');
+}*/
+
+let hasTooltip = document.querySelectorAll('.has-tooltip');
+
+let aText0 = hasTooltip.item(0).getAttribute('title');
+let aText1 = hasTooltip.item(1).getAttribute('title');
+let aText2 = hasTooltip.item(2).getAttribute('title');
+let aText3 = hasTooltip.item(3).getAttribute('title');
+let aText4 = hasTooltip.item(4).getAttribute('title');
+let aText5 = hasTooltip.item(5).getAttribute('title');
+
+hasTooltip.item(0).insertAdjacentHTML('beforeEnd',`<div class="tooltip">${aText0}</div>`);
+hasTooltip.item(1).insertAdjacentHTML('beforeEnd',`<div class="tooltip">${aText1}</div>`);
+hasTooltip.item(2).insertAdjacentHTML('beforeEnd',`<div class="tooltip">${aText2}</div>`);
+hasTooltip.item(3).insertAdjacentHTML('beforeEnd',`<div class="tooltip">${aText3}</div>`);
+hasTooltip.item(4).insertAdjacentHTML('beforeEnd',`<div class="tooltip">${aText4}</div>`);
+hasTooltip.item(5).insertAdjacentHTML('beforeEnd',`<div class="tooltip">${aText5}</div>`);
+
+let tooltip = document.querySelectorAll('.tooltip');
 
 
-hasTooltip.addEventListener(
+hasTooltip.item(0).addEventListener(
+  "click",  
+  () => {     
+    tooltip.item(0).classList.toggle('tooltip_active');
+  }
+)
+
+hasTooltip.item(1).addEventListener(
   "click",  
   () => { 
-    hasTooltip.appendChild(elementHelp);
-    let tooltip = document.querySelector('.tooltip');
-    tooltip.classList.toggle('tooltip_active');
-
+    tooltip.item(1).classList.toggle('tooltip_active');
+  }
+)
+hasTooltip.item(2).addEventListener(
+  "click",  
+  () => { 
+    tooltip.item(2).classList.toggle('tooltip_active');
+  }
+)
+hasTooltip.item(3).addEventListener(
+  "click",  
+  () => { 
+    tooltip.item(3).classList.toggle('tooltip_active');
+  }
+)
+hasTooltip.item(4).addEventListener(
+  "click",  
+  () => { 
+    tooltip.item(4).classList.toggle('tooltip_active');
+  }
+)
+hasTooltip.item(5).addEventListener(
+  "click",  
+  () => { 
+    tooltip.item(5).classList.toggle('tooltip_active');
   }
 )
 
