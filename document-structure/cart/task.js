03514abc8c0a308c20d81988ed0a9746032e1a9c
Maksim Chalyh <nazgulius@yandex.ru>
2024-07-19 23:18:33 +0300

    document-structure fixed final

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -2,14 +2,9 @@ let productQuantityValue = document.querySelectorAll('.product__quantity-value')
 let productQuantityControlDec = Array.from(document.querySelectorAll('.product__quantity-control_dec'));
 let productQuantityControlInc = Array.from(document.querySelectorAll('.product__quantity-control_inc'));
 let cartProducts = document.querySelector('.cart__products');
-//let cartProduct = Array.from(document.getElementsByClassName('cart__product'));
 let productAdd = Array.from(document.querySelectorAll('.product__add'));
-let products = document.querySelector('.products');
 let product = document.querySelectorAll('.product');
 let productImage = document.querySelectorAll('.product__image');
-let cartProductCount = document.getElementsByClassName('cart__product-count');
-
-let cartsArr = [];
 
 //  создание карточки товара в корзине
 function addProduct (cartId, cartSrc, cartCount) {
@@ -20,14 +15,9 @@ function addProduct (cartId, cartSrc, cartCount) {
         <a href="#" class="cart__remove">&times;</a>
     </div>`                
   );
-  /*cartsArr.push({
-      id: cartId,
-      src: cartSrc,
-      count: cartCount
-  }); */
 }
 
-// + - товара
+// + товара
 productQuantityControlDec.forEach((element, index) => { 
   element.addEventListener(
   "click",  
@@ -38,6 +28,7 @@ productQuantityControlDec.forEach((element, index) => {
     });
 });
 
+// - товара
 productQuantityControlInc.forEach((element, index) => { 
   element.addEventListener(
   "click",  
@@ -45,7 +36,8 @@ productQuantityControlInc.forEach((element, index) => {
       productQuantityValue[index].textContent = Number(productQuantityValue[index].textContent) + 1;
     });
 });
-  
+
+// логика добавления товара
 productAdd.forEach((element, index) => { 
   element.addEventListener(
   "click",  
@@ -53,33 +45,13 @@ productAdd.forEach((element, index) => {
       let cartId = product[index].dataset.id;
       let cartSrc = productImage[index].src;
       let cartCount = Number(productQuantityValue[index].textContent); 
-
       let cart = Array.from(document.getElementsByClassName('cart__product'));
       
       let productInCard = cart.find((element) => { return element.dataset.id === cartId; });
-      /*
-      cart.forEach((e, i) => {
-        console.log(e);
-      })*/
-
+      
       if(productInCard) {
-        
-        let p = Array.from(document.getElementsByClassName('cart__product-count'));
-        console.log('p ' + p[1]);
-        
-        p.forEach((e) => {
-          
-          console.log('до' + e.textContent);
-          e.textContent = Number(e.textContent) + Number(cartCount);
-          console.log('после' + e.textContent);
-  
-        })
-
-        console.log(productInCard.textContent);
-        console.log(Number(productInCard.textContent) + Number(cartCount));
-        
-        productInCard = Number(p) + Number(cartCount)
-
+        let a = Array.from(productInCard.getElementsByClassName('cart__product-count'))
+        a[0].innerText = Number(a[0].innerText) + cartCount;
       } else {
         addProduct(cartId, cartSrc, cartCount);
       }
