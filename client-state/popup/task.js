d8691f82a288dd2f59133a4cf29c6aa311b5c579
Maksim Chalyh <nazgulius@yandex.ru>
2024-08-06 23:05:50 +0300

    client-state, 2 - begin

--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -1,2 +1,6 @@
 const subscribeModal = document.getElementById('subscribe-modal');
+const modalClose = document.getElementsByClassName('modal__close');
 
+modalClose[0].addEventListener('click', (e) => {
+  subscribeModal.classList.remove('modal_active');
+})
